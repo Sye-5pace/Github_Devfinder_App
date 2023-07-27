037fd5327fc437469f76f861f9180c005bc8c94c
Sye-5pace <owusilasyp00@gmail.com>
2023-07-27 09:12:41 +0100

    Api request created with props drilled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import './index.css';
-import SearchUser from './SearchCard';
-import UserDetails from './UserDetails';
+import SearchUser from './SearchCard.tsx';
+import UserDetails from './UserDetails.tsx';
+import { UserData } from './interface.ts';
 // import LightSvg from './assets/light.svg';
 
 const App = () => {
+  const [ data, setData ] = useState(null)
+
+    
+  const handleApiRequest = async () => {
+    const userEntry:string  = 'https://api.github.com/users';
+
+    try{
+      const response = await fetch(userEntry);
+      if(!response.ok){
+        throw new Error("Request failed with status: " + response.status)
+      }
+      const users: UserData[] = await response.json();
+      console.log(users)
+    }catch(error){
+      console.log(error)
+    }
+  }
+
   return (
     <div className='bg-[#f4f9ff] w-full min-h-screen border-box p-0 m-0 font-space flex flex-row items-center justify-center'>
       <div className=' w-[55%] flex flex-col gap-y-6'>
@@ -14,13 +33,13 @@ const App = () => {
           </h1>
           <div className='flex flex-row items-center gap-4'>
             <p className='font-semibold text-[0.8rem] text-[#4f6c99]'> DARK</p>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 fill-[#4f6c99]">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 fill-[#4f6c99] cursor-pointer">
               <path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clip-rule="evenodd" />
             </svg>
           </div>
         </header>
-        <SearchUser/>
-        <UserDetails/>
+        <SearchUser /* search={} */ handleRequest={handleApiRequest}/>
+        <UserDetails />
       </div>
     </div>
   );
--- src/SearchCard.tsx
+++ src/SearchCard.tsx
@@ -1,6 +1,6 @@
 import React,{useState ,useEffect} from 'react'
 
-const SearchUser = () => {
+const SearchUser = ({ search, handleRequest }) => {
     const [user,setUser] = useState("")
 
 
@@ -11,7 +11,7 @@ const SearchUser = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
                 <input className='focus:outline-none font-semibold text-[#bbbec3] border-[#F57922] border-2 w-[23rem] h-full text-[1.2rem]'  placeholder='Search Github username...'/>
-                <button className='h-[3.4rem] w-[8.2rem] font-medium bg-[#017bfc] text-[#91cbec] rounded-[0.5rem] px-4'>Search</button>
+                <button className='h-[3.4rem] w-[8.2rem] font-medium bg-[#017bfc] text-[#91cbec] rounded-[0.5rem] px-4 cursor-pointer' onClick={handleRequest}>Search</button>
             </section>
         </div>
     );
